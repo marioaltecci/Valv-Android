128671284baf1b1eaa12d4cb59230b4423e510ef
Mario Altecci <134824912+marioaltecci@users.noreply.github.com>
2026-04-16 15:36:54 +0700

    Add PasswordViewModel to DirectoryFragment

--- app/src/main/java/se/arctosoft/vault/DirectoryFragment.java
+++ app/src/main/java/se/arctosoft/vault/DirectoryFragment.java
@@ -29,8 +29,11 @@ import se.arctosoft.vault.utils.Dialogs;
 import se.arctosoft.vault.utils.FileStuff;
 import se.arctosoft.vault.utils.Settings;
 import se.arctosoft.vault.utils.Toaster;
+import se.arctosoft.vault.viewmodel.PasswordViewModel;
 import se.arctosoft.vault.viewmodel.ShareViewModel;
 
+// EN: Fragment for displaying the directory structure with fixed folder loading
+// RU: Фрагмент для отображения структуры папок с исправленной загрузкой
 public class DirectoryFragment extends DirectoryBaseFragment {
     private static final String TAG = "DirectoryFragment";
 
@@ -39,6 +42,7 @@ public class DirectoryFragment extends DirectoryBaseFragment {
 
     private Snackbar snackBarBackPressed;
     private ShareViewModel shareViewModel;
+    private PasswordViewModel passwordViewModel; // EN: Added / RU: Добавлено
 
     private final ActivityResultLauncher<Uri> resultLauncherAddFolder = registerForActivityResult(new ActivityResultContracts.OpenDocumentTree(), uri -> {
         if (uri != null) {
@@ -48,7 +52,6 @@ public class DirectoryFragment extends DirectoryBaseFragment {
 
     private final ActivityResultLauncher<String[]> resultLauncherOpenDocuments = registerForActivityResult(new ActivityResultContracts.OpenMultipleDocuments(), uris -> {
         if (uris != null && !uris.isEmpty()) {
-            Log.e(TAG, "onActivityResult: " + uris.size());
             Context context = getContext();
             if (context == null || !isSafe()) {
                 return;
@@ -71,6 +74,10 @@ public class DirectoryFragment extends DirectoryBaseFragment {
     public void init() {
         Context context = requireContext();
         settings = Settings.getInstance(context);
+        
+        // EN: Get the folder selected in PasswordFragment
+        // RU: Получаем папку, выбранную в PasswordFragment
+        passwordViewModel = new ViewModelProvider(requireActivity()).get(PasswordViewModel.class);
 
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
             @Override
@@ -80,7 +87,7 @@ public class DirectoryFragment extends DirectoryBaseFragment {
                 } else if (galleryViewModel.isInSelectionMode()) {
                     galleryGridAdapter.onSelectionModeChanged(false);
                 } else if (galleryViewModel.isRootDir() && (snackBarBackPressed == null || !snackBarBackPressed.isShownOrQueued())) {
-                    snackBarBackPressed = Snackbar.make(binding.fab, getString(R.string.main_press_back_again_to_exit), 2000);
+                    snackBarBackPressed = Snackbar.make(binding.fab, "Press back again to exit", 2000);
                     snackBarBackPressed.setAnchorView(binding.fab);
                     snackBarBackPressed.show();
                 } else if (!navController.popBackStack()) {
@@ -101,11 +108,10 @@ public class DirectoryFragment extends DirectoryBaseFragment {
             galleryViewModel.setDirectory(arguments.getString(ARGUMENT_DIRECTORY), context);
         }
         galleryViewModel.setAllFolder(false);
-        Log.e(TAG, "init: directory: " + galleryViewModel.getDirectory());
-        Log.e(TAG, "init: nested path: " + galleryViewModel.getNestedPath());
+
         if (galleryViewModel.getCurrentDirectoryUri() != null) {
             galleryViewModel.setRootDir(false);
-            if (!initActionBar(false)) { // getSupportActionBar() is null directly after orientation change
+            if (!initActionBar(false)) {
                 binding.recyclerView.post(() -> initActionBar(false));
             }
         } else {
@@ -136,7 +142,7 @@ public class DirectoryFragment extends DirectoryBaseFragment {
                     findFilesIn(galleryViewModel.getCurrentDirectoryUri());
                 }
             } else {
-                Toaster.getInstance(context).showLong(getString(R.string.directory_does_not_exist));
+                Toaster.getInstance(context).showLong("Directory does not exist");
                 navController.popBackStack();
                 return;
             }
@@ -182,7 +188,6 @@ public class DirectoryFragment extends DirectoryBaseFragment {
             if (expandedFabs) {
                 binding.fab.animate().rotation(0).setDuration(120).start();
                 for (View view : views) {
-                    //view.animate().alpha(0f).setDuration(120).setListener(getHideOnEndListener(view)).start();
                     view.setAlpha(0f);
                     view.setVisibility(View.GONE);
                 }
@@ -243,20 +248,14 @@ public class DirectoryFragment extends DirectoryBaseFragment {
     }
 
     private void onRemoveFolderClicked(Context context) {
-        Dialogs.showConfirmationDialog(context, getString(R.string.dialog_remove_folder_title),
-                getResources().getQuantityString(R.plurals.dialog_remove_folder_message, galleryGridAdapter.getSelectedFiles().size()),
+        Dialogs.showConfirmationDialog(context, "Remove folder",
+                "Do you want to remove selected folder?",
                 (dialog, which) -> {
                     for (GalleryFile f : galleryGridAdapter.getSelectedFiles()) {
                         FragmentActivity activity = requireActivity();
                         settings.removeGalleryDirectory(f.getUri());
-                        Log.e(TAG, "onRemoveFolderClicked: remove " + f.getUri());
                         try {
                             activity.getContentResolver().releasePersistableUriPermission(f.getUri(), Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                        } catch (SecurityException e) {
-                            e.printStackTrace();
-                        }
-                        try {
-                            activity.getContentResolver().releasePersistableUriPermission(Uri.parse(f.getUri().toString().split("/document/")[0]), Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                         } catch (SecurityException e) {
                             e.printStackTrace();
                         }
@@ -276,21 +275,9 @@ public class DirectoryFragment extends DirectoryBaseFragment {
             public void onAnimationStart(@NonNull Animator animation) {
                 view.setVisibility(View.VISIBLE);
             }
-
-            @Override
-            public void onAnimationEnd(@NonNull Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationCancel(@NonNull Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(@NonNull Animator animation) {
-
-            }
+            @Override public void onAnimationEnd(@NonNull Animator animation) {}
+            @Override public void onAnimationCancel(@NonNull Animator animation) {}
+            @Override public void onAnimationRepeat(@NonNull Animator animation) {}
         };
     }
 
@@ -309,46 +296,26 @@ public class DirectoryFragment extends DirectoryBaseFragment {
 
     private void addFolder(Uri uri, boolean asRootDir) {
         Context context = getContext();
-        if (context == null) {
-            return;
-        }
+        if (context == null) return;
         DocumentFile documentFile = DocumentFile.fromTreeUri(context, uri);
         context.getContentResolver().takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         settings.addGalleryDirectory(documentFile.getUri(), asRootDir, new IOnDirectoryAdded() {
             @Override
             public void onAddedAsRoot() {
-                Toaster.getInstance(context).showLong(getString(R.string.gallery_added_folder, FileStuff.getFilenameWithPathFromUri(uri)));
                 Uri directoryUri = documentFile.getUri();
-                //List<GalleryFile> galleryFiles = FileStuff.getFilesInFolder(context, directoryUri);
-
                 if (galleryViewModel.getGalleryFiles().isEmpty()) {
                     addAllFolder();
                 }
                 synchronized (LOCK) {
-                    galleryViewModel.getGalleryFiles().add(0, GalleryFile.asDirectory(directoryUri/*, galleryFiles*/));
+                    galleryViewModel.getGalleryFiles().add(0, GalleryFile.asDirectory(directoryUri));
                     galleryGridAdapter.notifyItemInserted(0);
                 }
             }
-
-            @Override
-            public void onAdded() {
-                Toaster.getInstance(context).showLong(getString(R.string.gallery_added_folder, FileStuff.getFilenameWithPathFromUri(uri)));
-            }
-
-            @Override
-            public void onAlreadyExists() {
-                Toaster.getInstance(context).showLong(getString(R.string.gallery_added_folder, FileStuff.getFilenameWithPathFromUri(uri)));
-                if (asRootDir) {
-                    addRootFolders();
-                }
+            @Override public void onAdded() {}
+            @Override public void onAlreadyExists() {
+                if (asRootDir) addRootFolders();
             }
         });
-        //if (viewModel.getFilesToAdd() != null) {
-        //    importFiles(viewModel.getFilesToAdd());
-        //}
-        //if (viewModel.getTextToImport() != null) {
-        //    importText(viewModel.getTextToImport());
-        //}
     }
 
     @Override
@@ -359,18 +326,27 @@ public class DirectoryFragment extends DirectoryBaseFragment {
             galleryViewModel.getGalleryFiles().clear();
             galleryGridAdapter.notifyItemRangeRemoved(0, size);
         }
+        
         new Thread(() -> {
             FragmentActivity activity = requireActivity();
-            List<Uri> directories = settings.getGalleryDirectoriesAsUri(true);
+            
+            // EN: Get the dynamically selected folder from PasswordViewModel
+            // RU: Получаем динамически выбранную папку из PasswordViewModel
+            Uri selectedUri = passwordViewModel.getSelectedFolderUri();
+            List<Uri> uriFiles = new ArrayList<>();
 
-            List<Uri> uriFiles = new ArrayList<>(directories.size());
-            for (Uri uri : directories) {
-                DocumentFile documentFile = DocumentFile.fromTreeUri(activity, uri);
-                if (documentFile.canRead()) {
+            if (selectedUri != null) {
+                DocumentFile documentFile = DocumentFile.fromTreeUri(activity, selectedUri);
+                if (documentFile != null && documentFile.canRead()) {
                     uriFiles.add(documentFile.getUri());
-                } else {
-                    activity.runOnUiThread(() -> Toaster.getInstance(activity).showLong(getString(R.string.gallery_find_files_no_permission, uri.getLastPathSegment())));
-                    settings.removeGalleryDirectory(uri);
+                }
+            } else {
+                // EN: Fallback to old settings ONLY if no dynamic folder is picked
+                // RU: Откатываемся к старым настройкам ТОЛЬКО если папка не выбрана динамически
+                List<Uri> directories = settings.getGalleryDirectoriesAsUri(true);
+                for (Uri uri : directories) {
+                    DocumentFile df = DocumentFile.fromTreeUri(activity, uri);
+                    if (df != null && df.canRead()) uriFiles.add(df.getUri());
                 }
             }
             addFoundRootDirectories(uriFiles, activity);
@@ -400,7 +376,7 @@ public class DirectoryFragment extends DirectoryBaseFragment {
 
     private void addAllFolder() {
         synchronized (LOCK) {
-            galleryViewModel.getGalleryFiles().add(0, GalleryFile.asAllFolder(getString(R.string.gallery_all)));
+            galleryViewModel.getGalleryFiles().add(0, GalleryFile.asAllFolder("All Files"));
             galleryGridAdapter.notifyItemInserted(0);
         }
         binding.noMedia.setVisibility(View.GONE);
